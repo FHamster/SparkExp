2cb44b646f607f3dc50b7bd10814d9c34fe966b9
FHamster <gaoxin459716010@163.com>
2020-04-05 16:44:37 +0800

    添加了导入mongodb的测试

--- src/test/scala/MongoSparkTest.scala
+++ src/test/scala/MongoSparkTest.scala
@@ -8,21 +8,28 @@ import org.scalatest.funsuite.AnyFunSuite
  */
 class MongoSparkTest extends AnyFunSuite {
   val testFile = "src/test/resources/article_after.xml"
-  val testFile2 = "src/test/resources/article_CharTest.xml"
-  val spark = SparkSession.builder
+
+  val subNodeName = "article"
+
+  val charTest = "src/test/resources/article_CharTest.xml"
+  val spark: SparkSession = SparkSession
+    .builder
     .appName("XML_WriteTest")
     .master("local[*]")
-    .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/scalaTest.test3")
+    .config("spark.mongodb.output.uri", s"mongodb://127.0.0.1/SparkDBLPTest.$subNodeName")
     .getOrCreate()
-  test("write into mongodb") {
+  test("write article subnode into mongodb") {
     import com.databricks.spark.xml._
     val opt = spark.read
       .option("rootTag", "dblp")
-      .option("rowTag", "article")
+      .option("rowTag", subNodeName)
       .option("charset", "UTF-8")
-      .xml(testFile2)
+      .xml(PropertiesObj.wholeDBLP_cvtSparkPath)
 
     opt.show()
+    opt.printSchema()
+
+    println("write into mongodb")
     MongoSpark.save(opt)
   }
 }
